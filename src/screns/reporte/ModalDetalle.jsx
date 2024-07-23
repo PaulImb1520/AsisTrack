fac72ef23cc02a191cc8102a634d256c76ff64c2
Paúl Imbaquingo <paul.imbm@gmail.com>
2024-07-23 10:03:42 -0500

    Corrección de carga de datos hacia el modal

--- src/screns/reporte/ModalDetalle.jsx
+++ src/screns/reporte/ModalDetalle.jsx
@@ -3,23 +3,21 @@ import { getSingleEmpleadoData } from "../../services/reportService";
 import { Button, Col, Grid, Modal, Row } from "rsuite";
 
 export const ModalDetalle = ({ open, setOpen, employeeNo, reporte }) => {
-  const [modalLabels, setModalLabels] = useState([]);
+  const [modalLabels, setModalLabels] = useState(null);
+
   useEffect(() => {
-    if (open == true) {
+    if (open) {
       const selectedEmployeeData = reporte.find(
         (obj) => obj.employeeNo === employeeNo
       );
 
       getSingleEmpleadoData(employeeNo)
         .then((res) => {
-          console.log(res.data);
-          let aux = [];
-          aux.push({
+          const aux = {
             ...res.data,
             ...selectedEmployeeData,
-          });
+          };
           setModalLabels(aux);
-          console.log(aux);
         })
         .catch((error) => {
           console.log(error);
@@ -31,85 +29,92 @@ export const ModalDetalle = ({ open, setOpen, employeeNo, reporte }) => {
     let valorDia = parseFloat(value) / 30;
     let valorHora = valorDia / 8;
     let valorHoraExtra = valorHora * 1.5;
-    console.log;
-    return (modalLabels[0].overTimeHours * valorHoraExtra).toFixed(2);
+    return (modalLabels.overTimeHours * valorHoraExtra).toFixed(2);
   };
 
   return (
     <>
-      <Modal open={open} size="sm">
-        <Modal.Title className="modalTitle">Detalle</Modal.Title>
-        <Modal.Header closeButton={false} />
-        <Modal.Body className="modal-body">
-          <Grid style={{ width: "100%" }}>
-            <h4 style={{ paddingBottom: "16px" }}>Identidad</h4>
-            <Row>
-              <Col lg={3} className="col-title">
-                Nombre:
-              </Col>
-              <Col lg={12} className="col-center">
-                {modalLabels[0].nombre_completo}
-              </Col>
-              <Col lg={3} className="col-title">
-                Cédula:
-              </Col>
-              <Col lg={6} className="col-center">
-                {modalLabels[0].cedula}
-              </Col>
-            </Row>
-            <h4 style={{ paddingBottom: "16px", paddingTop: "32px" }}>Horas</h4>
-            <Row className="row">
-              <Col lg={12} className="col-title">
-                Horas Regulares
-              </Col>
-              <Col lg={12} className="col-right">
-                {modalLabels[0].actualHours + " hrs. /mes"}
-              </Col>
-              <Col lg={12} className="col-title">
-                Horas extras (+50%):
-              </Col>
-              <Col lg={12} className="col-right">
-                {modalLabels[0].overTimeHours + " hrs. /mes"}
-              </Col>
-            </Row>
-            <h4 style={{ paddingBottom: "16px", paddingTop: "32px" }}>
-              Salario
-            </h4>
-            <Row>
-              <Col lg={12} className="col-title">
-                Salario Mensual:
-              </Col>
-              <Col lg={12} className="col-right">
-                {modalLabels[0].sueldo}
-              </Col>
-            </Row>
-            <Row className="row">
-              <Col lg={12} className="col-title">
-                Salario por Hora (calculado):
-              </Col>
-              <Col lg={12} className="col-right">
-                {modalLabels[0].salario_hora}
-              </Col>
-            </Row>
-            <Row
-              style={{ paddingBottom: "16px", paddingTop: "32px" }}
-              className="row"
-            >
-              <Col lg={12} className="col-payment-title">
-                <h4>Salario extra por pagar: </h4>
-              </Col>
-              <Col lg={12} className="col-payment-title col-right">
-                {calculateSalary(modalLabels[0].sueldo)}
-              </Col>
-            </Row>
-          </Grid>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button appearance="primary" onClick={() => setOpen(false)}>
-            Regresar
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      {open ? (
+        <Modal open={open} size="sm">
+          <Modal.Title className="modalTitle">Detalle</Modal.Title>
+          <Modal.Header closeButton={false} />
+          <Modal.Body className="modal-body">
+            {modalLabels ? (
+              <Grid style={{ width: "100%" }}>
+                <h4 style={{ paddingBottom: "16px" }}>Identidad</h4>
+                <Row>
+                  <Col lg={3} className="col-title">
+                    Nombre:
+                  </Col>
+                  <Col lg={12} className="col-center">
+                    {modalLabels.nombre_completo}
+                  </Col>
+                  <Col lg={3} className="col-title">
+                    Cédula:
+                  </Col>
+                  <Col lg={6} className="col-center">
+                    {modalLabels.cedula}
+                  </Col>
+                </Row>
+                <h4 style={{ paddingBottom: "16px", paddingTop: "32px" }}>
+                  Horas
+                </h4>
+                <Row className="row">
+                  <Col lg={12} className="col-title">
+                    Horas Regulares
+                  </Col>
+                  <Col lg={12} className="col-right">
+                    {modalLabels.actualHours + " hrs. /mes"}
+                  </Col>
+                  <Col lg={12} className="col-title">
+                    Horas extras (+50%):
+                  </Col>
+                  <Col lg={12} className="col-right">
+                    {modalLabels.overTimeHours + " hrs. /mes"}
+                  </Col>
+                </Row>
+                <h4 style={{ paddingBottom: "16px", paddingTop: "32px" }}>
+                  Salario
+                </h4>
+                <Row>
+                  <Col lg={12} className="col-title">
+                    Salario Mensual:
+                  </Col>
+                  <Col lg={12} className="col-right">
+                    {modalLabels.sueldo}
+                  </Col>
+                </Row>
+                <Row className="row">
+                  <Col lg={12} className="col-title">
+                    Salario por Hora (calculado):
+                  </Col>
+                  <Col lg={12} className="col-right">
+                    {modalLabels.salario_hora}
+                  </Col>
+                </Row>
+                <Row
+                  style={{ paddingBottom: "16px", paddingTop: "32px" }}
+                  className="row"
+                >
+                  <Col lg={12} className="col-payment-title">
+                    <h4>Salario extra por pagar: </h4>
+                  </Col>
+                  <Col lg={12} className="col-payment-title col-right">
+                    {calculateSalary(modalLabels.sueldo)}
+                  </Col>
+                </Row>
+              </Grid>
+            ) : (
+              <p>Cargando datos...</p>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button appearance="primary" onClick={() => setOpen(false)}>
+              Regresar
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : null}
     </>
   );
 };
